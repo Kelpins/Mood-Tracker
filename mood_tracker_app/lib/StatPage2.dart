21607b493f3d338936ab736bcc3dbeffce4fd259
Kelpins <kellan@edgy.org>
2023-03-09 09:24:12 -0800

    Working UpperNavBar

---
+++ mood_tracker_app/lib/StatPage2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StatPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Text("Second Statistics Page"),
+    );
+  }
+}
--- mood_tracker_app/lib/statsPage.dart
+++ mood_tracker_app/lib/statsPage.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'StatPage1.dart';
+import 'StatPage2.dart';
+import 'StatPage3.dart';
 
 class StatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Statistics")),
-        ),
-        body: Column(
-          children: [
-            Text("This is the Statistics Page!!!"),
-            Text("This is where you view data"),
-          ],
-        ));
+    return DefaultTabController(
+        initialIndex: 0,
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Center(child: Text("Statistics")),
+                bottom: const TabBar(
+                  tabs: <Widget>[
+                    Tab(
+                      icon: Icon(Icons.face),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.beach_access_sharp),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.heart_broken),
+                    ),
+                  ],
+                )),
+            body: TabBarView(
+              children: <Widget>[
+                StatPage1(),
+                StatPage2(),
+                StatPage3(),
+              ],
+            )));
   }
 }
