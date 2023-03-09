313dd7277ccf660902c9a4320fe09219d202d9af
irist25 <irist25@lakesideschool.org>
2023-03-09 09:18:54 -0800

    Added tabs to the statistics page

--- mood_tracker_app/lib/statsPage.dart
+++ mood_tracker_app/lib/statsPage.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
 class StatsPage extends StatelessWidget {
+  const StatsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Statistics")),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.calendar_month)),
+                Tab(icon: Icon(Icons.polyline)),
+                Tab(icon: Icon(Icons.align_horizontal_left_rounded)),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.calendar_month),
+              Icon(Icons.polyline),
+              Icon(Icons.align_horizontal_left_rounded),
+            ],
+          ),
         ),
-        body: Column(
-          children: [
-            Text("This is the Statistics Page!!!"),
-            Text("This is where you view data"),
-          ],
-        ));
+      ),
+    );
   }
 }
