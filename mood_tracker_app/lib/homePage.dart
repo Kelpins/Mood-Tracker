d20fb207833155b247956141ad9c938dc435b013
Kelpins <kellan@edgy.org>
2023-03-13 09:30:09 -0700

    Home Page Update
    - Date & time fixed for top box
    - Gradient more visible for top box

--- mood_tracker_app/lib/homePage.dart
+++ mood_tracker_app/lib/homePage.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    String time = DateFormat("jm").format(now);
+    String month = DateFormat("MMMM").format(now);
+    String day = DateFormat('dd').format(now);
+    String weekday = DateFormat('EEEE').format(now);
+
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Home")),
         ),
-        body: Center(
-            child: Container(
-                decoration: BoxDecoration(
-                  gradient:
-                      LinearGradient(colors: [Colors.blue, Colors.purple]),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                margin: const EdgeInsets.all(10.0),
-                width: 250.0,
-                height: 100.0,
-                child: Column(
-                  children: [
-                    Text("Today is Tuesday, March 7th"),
-                    Text("Element 2"),
-                  ],
-                ))));
+        body: Column(children: [
+          Center(
+              child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 57, 150, 227),
+                      Color.fromARGB(255, 165, 72, 182)
+                    ]),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  margin: const EdgeInsets.all(10.0),
+                  width: 350.0,
+                  height: 75.0,
+                  child: Center(
+                    child: Text(
+                      "It is $time on $weekday, $month $day!",
+                      textScaleFactor: 1.25,
+                    ),
+                  )))
+        ]));
   }
 }
