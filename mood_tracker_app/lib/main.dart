84ac5c75bfbcc85bd9677dc190279bde64212e73
Kelpins <kellan@edgy.org>
2023-03-29 09:42:29 -0700

    Firebase Setup Stuff
    I followed the tutorials and this is what it told me to add.

--- mood_tracker_app/lib/main.dart
+++ mood_tracker_app/lib/main.dart
@@ -4,7 +4,16 @@ import 'homePage.dart';
 import 'statsPage.dart';
 import 'profile.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
