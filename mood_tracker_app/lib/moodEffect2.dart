7b2b77466a647c73391e7dc6afb34b5107c615c8
Iris <irist25@lakesideschool.org>
2023-06-02 12:46:10 -0700

    working vertical bar charts (ugly)

--- mood_tracker_app/lib/moodEffect2.dart
+++ mood_tracker_app/lib/moodEffect2.dart
@@ -1,6 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'dart:async';
+import 'dart:io';
+
 import 'dart:math' as math;
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/material.dart';
+
+// this version technically works but it is so incredibly ugly.
+// the bars are vertical and all the labels are overlapping.
+// this is awful
 
 class AppColors {
   static const Color primary = contentColorCyan;
@@ -51,17 +63,16 @@ class BarChartSample5 extends StatefulWidget {
 }
 
 class BarChartSample5State extends State<BarChartSample5> {
+  var user = FirebaseAuth.instance.currentUser!;
+  var habitList = [];
+  var moodDays = [];
+  var barLabels = [];
+  var barValues = [];
+
   static const double barWidth = 22;
   static const shadowOpacity = 0.0;
-  static const mainItems = <int, List<double>>{
-    0: [0.1],
-    1: [-0.3],
-    2: [0.4],
-    3: [0.6],
-    4: [-0.7],
-    5: [-1],
-    6: [0.5],
-  };
+  var mainItems = <int, List<double>>{};
+
   int touchedIndex = -1;
 
   @override
@@ -69,6 +80,7 @@ class BarChartSample5State extends State<BarChartSample5> {
     super.initState();
   }
 
+/*
   Widget bottomTitles(double value, TitleMeta meta) {
     const style = TextStyle(color: Colors.black, fontSize: 10);
     String text;
@@ -103,36 +115,12 @@ class BarChartSample5State extends State<BarChartSample5> {
       child: Text(text, style: style),
     );
   }
+  */
 
   Widget topTitles(double value, TitleMeta meta) {
     const style = TextStyle(color: Colors.black, fontSize: 10);
     String text;
-    switch (value.toInt()) {
-      case 0:
-        text = 'Mon';
-        break;
-      case 1:
-        text = 'Tue';
-        break;
-      case 2:
-        text = 'Wed';
-        break;
-      case 3:
-        text = 'Thu';
-        break;
-      case 4:
-        text = 'Fri';
-        break;
-      case 5:
-        text = 'Sat';
-        break;
-      case 6:
-        text = 'Sun';
-        break;
-      default:
-        text = '';
-        return Container();
-    }
+    text = barLabels[value.toInt()];
     return SideTitleWidget(
       axisSide: meta.axisSide,
       child: Text(text, style: style),
@@ -246,102 +234,312 @@ class BarChartSample5State extends State<BarChartSample5> {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 0.8,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 16),
-        child: BarChart(
-          BarChartData(
-            alignment: BarChartAlignment.center,
-            maxY: 1,
-            minY: -1,
-            groupsSpace: 12,
-            barTouchData: BarTouchData(
-              handleBuiltInTouches: false,
-              touchCallback: (FlTouchEvent event, barTouchResponse) {
-                if (!event.isInterestedForInteractions ||
-                    barTouchResponse == null ||
-                    barTouchResponse.spot == null) {
-                  setState(() {
-                    touchedIndex = -1;
-                  });
-                  return;
-                }
-                final rodIndex = barTouchResponse.spot!.touchedRodDataIndex;
-                if (isShadowBar(rodIndex)) {
-                  setState(() {
-                    touchedIndex = -1;
-                  });
-                  return;
-                }
-                setState(() {
-                  touchedIndex = barTouchResponse.spot!.touchedBarGroupIndex;
-                });
-              },
-            ),
-            titlesData: FlTitlesData(
-              show: true,
-              topTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  reservedSize: 32,
-                  getTitlesWidget: topTitles,
-                ),
-              ),
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  reservedSize: 32,
-                  getTitlesWidget: bottomTitles,
-                ),
-              ),
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: leftTitles,
-                  interval: 5,
-                  reservedSize: 42,
-                ),
-              ),
-              rightTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: rightTitles,
-                  interval: 5,
-                  reservedSize: 42,
-                ),
-              ),
-            ),
-            gridData: FlGridData(
-              show: true,
-              checkToShowHorizontalLine: (value) => value % 5 == 0,
-              getDrawingHorizontalLine: (value) {
-                if (value == 0) {
-                  return FlLine(
-                    color: AppColors.borderColor.withOpacity(0.1),
-                    strokeWidth: 3,
+    // Iris's code starts here -----------------------------------
+    bool loaded = false;
+
+    final db = FirebaseFirestore.instance;
+    final email = user.email;
+
+    for (int i = 0; i < barValues.length; i++) {
+      mainItems.addAll({
+        i: [barValues[i]]
+      });
+    }
+
+    CollectionReference userDatabase =
+        FirebaseFirestore.instance.collection('Users');
+
+    // I could not find a built in version for dart :(
+    boolToInt(bool boolValue) {
+      if (boolValue == true) {
+        return 1;
+      }
+      return 0;
+    }
+
+    Future<num> calculateCorrelationFactor(
+        String habitName, List moodDays, moodData) async {
+      num correlationFactor = 0;
+      var habitDays = [];
+      var validDays = [];
+      var habitValues = [];
+      var moodValues = [];
+      num habitMean = 0;
+      num habitSum = 0;
+      var habitDeviationValues = [];
+      num habitStandardDeviation = 0;
+      num moodMean = 0;
+      num moodSum = 0;
+      var moodDeviationValues = [];
+      num moodStandardDeviation = 0;
+      num deviationProductSum = 0;
+
+      final habitDataRef =
+          db.collection('Users').doc(email).collection("Habits").doc(habitName);
+
+      //await habitDataRef.get().then(
+      //(DocumentSnapshot doc) {
+      final DocumentSnapshot doc = await habitDataRef.get();
+      final habitData = doc.data() as Map<String, dynamic>;
+
+      // Days where there is data for that specific habit
+      habitDays = habitData.keys.toList();
+
+      // Days where there is data for both mood and that habit
+      validDays = habitDays;
+      validDays.removeWhere((item) => !moodDays.contains(item));
+
+      // Goes through the days and finds the relevant habit and mood values
+      for (var i = 0; i < validDays.length; i++) {
+        habitValues.add(boolToInt(habitData[validDays[i]]));
+        moodValues.add(moodData[validDays[i]]);
+      }
+
+      // HABITS SECTION ----------------------------------------------------
+
+      // Mean habit value
+      for (num value in habitValues) {
+        habitSum += value;
+      }
+      habitMean = habitSum / habitValues.length;
+
+      // Finds the deviation score of each habit value (value - mean)
+      habitDeviationValues = List.filled(habitValues.length, 0);
+      for (var i = 0; i < habitValues.length; i++) {
+        habitDeviationValues[i] = habitValues[i] - habitMean;
+      }
+
+      // Finds the standard deviation of the habits (sum of the square of the deviation scores)
+      for (num value in habitDeviationValues) {
+        habitStandardDeviation += value * value;
+      }
+
+      // MOOD SECTION ----------------------------------------------------
+
+      // Mean mood value
+      for (num value in moodValues) {
+        moodSum += value;
+      }
+      moodMean = moodSum / moodValues.length;
+
+      // Finds the deviation score of each mood value (value - mean)
+      moodDeviationValues = List.filled(moodValues.length, 0);
+      for (var i = 0; i < moodValues.length; i++) {
+        moodDeviationValues[i] = moodValues[i] - moodMean;
+      }
+
+      // Finds the standard deviation of the moods (sum of the square of the deviation scores)
+      for (num value in moodDeviationValues) {
+        moodStandardDeviation += value * value;
+      }
+
+      // OVERALL SECTION ---------------------------------------------------
+
+      // Sum of products of deviation scores
+      for (var i = 0; i < moodDeviationValues.length; i++) {
+        deviationProductSum += moodDeviationValues[i] * habitDeviationValues[i];
+      }
+
+      // Finding the correlation factor
+      correlationFactor = deviationProductSum /
+          math.sqrt(moodStandardDeviation * habitStandardDeviation);
+      //},
+
+      //onError: (e) ==> print("Error getting document: $e"),
+      //);
+
+      return correlationFactor;
+    }
+
+    return FutureBuilder<DocumentSnapshot>(
+        future: userDatabase.doc(email).collection("Moods").doc("Mood").get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+//Error Handling conditions
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Text("Document does not exist");
+          }
+
+          loaded = true;
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Text("Document does not exist");
+          }
+
+          loaded = true;
+
+          //Data is output to the user
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> userData =
+                snapshot.data!.data() as Map<String, dynamic>;
+
+            // read database for list of habits
+            habitList = userData['Habits'];
+            print("Habit List: " + habitList.toString());
+
+            // read database for list of days w/ mood data
+            moodDays = userData.keys.toList();
+            moodDays.remove("username");
+            moodDays.remove("Habits");
+            moodDays.remove("HabitMatchingToday");
+            moodDays.remove("HabitDay");
+            print("Days with mood data: " + moodDays.toString());
+
+            // iterates through all the habits
+
+            var correlationFactorFutures = <Future<num>>[];
+            for (int i = 0; i < habitList.length; i++) {
+              var habit = habitList[i];
+              // finds the correlation between the specific habit and the user's mood
+              // this will return NaN if there's not enough data!! do not worry!!
+              var correlationFactorFuture =
+                  calculateCorrelationFactor(habit, moodDays, userData);
+              correlationFactorFutures.add(correlationFactorFuture);
+            }
+
+            return FutureBuilder<List<num>>(
+              future: Future.wait(correlationFactorFutures),
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<num>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  barLabels.clear();
+                  barValues.clear();
+                  for (int i = 0; i < habitList.length; i++) {
+                    var correlationFactor = snapshot.data![i];
+                    barLabels.add(habitList[i].toString());
+                    barValues.add(correlationFactor.toDouble());
+                    mainItems[i] = [correlationFactor.toDouble()];
+                    print("CF: $correlationFactor");
+                  }
+                  print("values!!!: $barValues");
+
+                  return AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: BarChart(
+                        BarChartData(
+                          alignment: BarChartAlignment.center,
+                          maxY: 1,
+                          minY: -1,
+                          groupsSpace: 12,
+                          barTouchData: BarTouchData(
+                            handleBuiltInTouches: false,
+                            touchCallback:
+                                (FlTouchEvent event, barTouchResponse) {
+                              if (!event.isInterestedForInteractions ||
+                                  barTouchResponse == null ||
+                                  barTouchResponse.spot == null) {
+                                setState(() {
+                                  touchedIndex = -1;
+                                });
+                                return;
+                              }
+                              final rodIndex =
+                                  barTouchResponse.spot!.touchedRodDataIndex;
+                              if (isShadowBar(rodIndex)) {
+                                setState(() {
+                                  touchedIndex = -1;
+                                });
+                                return;
+                              }
+                              setState(() {
+                                touchedIndex =
+                                    barTouchResponse.spot!.touchedBarGroupIndex;
+                              });
+                            },
+                          ),
+                          titlesData: FlTitlesData(
+                            show: true,
+                            topTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 32,
+                                getTitlesWidget: topTitles,
+                              ),
+                            ),
+                            /*
+                            bottomTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 32,
+                                getTitlesWidget: bottomTitles,
+                              ),
+                            ),
+                            */
+                            leftTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: leftTitles,
+                                interval: 5,
+                                reservedSize: 42,
+                              ),
+                            ),
+                            rightTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: rightTitles,
+                                interval: 5,
+                                reservedSize: 42,
+                              ),
+                            ),
+                          ),
+                          gridData: FlGridData(
+                            show: true,
+                            checkToShowHorizontalLine: (value) =>
+                                value % 5 == 0,
+                            getDrawingHorizontalLine: (value) {
+                              if (value == 0) {
+                                return FlLine(
+                                  color: AppColors.borderColor.withOpacity(0.1),
+                                  strokeWidth: 3,
+                                );
+                              }
+                              return FlLine(
+                                color: AppColors.borderColor.withOpacity(0.05),
+                                strokeWidth: 0.8,
+                              );
+                            },
+                          ),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          barGroups: mainItems.entries
+                              .map(
+                                (e) => generateGroup(
+                                  e.key,
+                                  e.value[0],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ),
                   );
                 }
-                return FlLine(
-                  color: AppColors.borderColor.withOpacity(0.05),
-                  strokeWidth: 0.8,
+                return Column(
+                  children: [
+                    SizedBox(height: 200),
+                    Center(
+                        child:
+                            const Text("Calculating correlation factors...")),
+                  ],
                 );
               },
-            ),
-            borderData: FlBorderData(
-              show: false,
-            ),
-            barGroups: mainItems.entries
-                .map(
-                  (e) => generateGroup(
-                    e.key,
-                    e.value[0],
-                  ),
-                )
-                .toList(),
-          ),
-        ),
-      ),
-    );
+            );
+          }
+          if (!loaded) {
+            return Column(children: [
+              SizedBox(height: 200),
+              Center(child: const Text("Loading..."))
+            ]);
+          }
+
+          return Scaffold();
+        });
   }
 }
